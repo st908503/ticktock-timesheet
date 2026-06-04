97434d63f6898600b02656ac3685bb31d68e22a1
Shashank Tripathi <caspersky@Shashanks-MacBook-Pro.local>
2026-06-04 12:11:21 +0530

    route fixed

--- src/app/api/entries/[id]/route.ts
+++ src/app/api/entries/[id]/route.ts
@@ -8,38 +8,35 @@ import {
   updateEntry,
 } from "@/lib/mock-db";
 
-type Params = {
-  params: Promise<{
-    id: string;
-  }>;
-};
-
 export async function PUT(
   request: NextRequest,
-  { params }: Params
+  context: {
+    params: Promise<{
+      id: string;
+    }>;
+  }
 ) {
-
-  // unwrap async params
-  const { id } = await params;
+  const { id } = await context.params;
 
   const body = await request.json();
 
-  const updated =
-    await updateEntry(
-      id,
-      body
-    );
+  const updated = await updateEntry(
+    id,
+    body
+  );
 
   return NextResponse.json(updated);
 }
 
 export async function DELETE(
   _: NextRequest,
-  { params }: Params
+  context: {
+    params: Promise<{
+      id: string;
+    }>;
+  }
 ) {
-
-  // unwrap async params
-  const { id } = await params;
+  const { id } = await context.params;
 
   await deleteEntry(id);
 
